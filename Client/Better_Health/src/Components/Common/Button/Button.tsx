e7c8d5c1b9e3b01245966aabed27c6f3254a279c
“[KiokoEric]” <[kiokoerick040@gmail.com]>
2024-09-21 23:42:48 +0300

    Intorduction of additional props to the reusable button component.

--- Client/Better_Health/src/Components/Common/Button/Button.tsx
+++ Client/Better_Health/src/Components/Common/Button/Button.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 interface ButtonProps {
     onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
-    Icon?: string;
-    ButtonText: string;
-    ButtonStyle: string;
+    Icon?: any;
+    ID?: string;
+    Children?: any;
+    ButtonText?: string;
+    ButtonStyle?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ Icon, ButtonText, ButtonStyle, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ ID, Icon, Children, ButtonText, ButtonStyle, onClick }) => {
 return (
-    <div onClick={onClick} className={ButtonStyle}> {Icon} { ButtonText }</div>
+    <div id={ID} onClick={onClick} className={ButtonStyle}> {Icon} {Children} { ButtonText }</div>
 )
 }
 
