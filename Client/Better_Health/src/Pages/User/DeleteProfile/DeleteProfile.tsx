6bd3b915f355119ff601a4bb17178e8feb637a6d
“[KiokoEric]” <[kiokoerick040@gmail.com]>
2024-09-22 14:42:38 +0300

    Modification of the Delete Profile Page to be responsive in both mobile view and tablet view.

--- Client/Better_Health/src/Pages/User/DeleteProfile/DeleteProfile.tsx
+++ Client/Better_Health/src/Pages/User/DeleteProfile/DeleteProfile.tsx
@@ -1,13 +1,45 @@
+import axios from "axios";
 import React from 'react';
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import Button from '../../../Components/Common/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { useGetUserID } from "../../../Components/Hooks/useGetUserID";
 
 const DeleteProfile:React.FC = () => {
+
+    const userID = useGetUserID();
+    const navigate = useNavigate();
+    const [Cookie,setCookie] = useCookies(["auth_token"])  
+
+    // DELETE USER FUNCTION
+
+    const DeleteUser = (id: any) => {
+        try{
+            axios.delete(`http://localhost:4000/Users/Delete/${id}`, {
+                headers: { authorization: Cookie.auth_token }
+            })
+            .then(() => { 
+                navigate("/Registration")
+                setCookie("auth_token", "");
+                window.localStorage.clear()
+            })
+        }
+        catch (Error){
+            console.log(Error)
+        }
+    }
+
 return (
     <div id='DeleteProfile' className='flex flex-col gap-5 justify-center items-center' >
         <h1 className='font-bold text-5xl text-white'>We are sorry to see you go, but hope to see you again!</h1>
-        <button className='bg-black cursor-pointer flex items-center justify-center gap-4 text-center text-white px-2 py-1.5 rounded w-40' ><FontAwesomeIcon icon={faTrash} />Delete My Profile</button> 
+        <Button
+            onClick={()=>DeleteUser(userID)}
+            ButtonStyle="bg-black cursor-pointer flex items-center justify-center gap-4 text-center text-white px-2 py-1.5 rounded w-40"
+            ButtonText="Delete My Profile"
+            Children={<FontAwesomeIcon icon={faTrash} />}
+        /> 
     </div>
 )
 }
