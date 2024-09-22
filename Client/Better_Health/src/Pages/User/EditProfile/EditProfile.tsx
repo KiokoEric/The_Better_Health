b2da91488c8f3f33a9d1c46da62cd074b46f67f8
“[KiokoEric]” <[kiokoerick040@gmail.com]>
2024-09-22 15:07:41 +0300

    Modification of the Edit Profile Page to be responsive in both mobile view and tablet view.

--- Client/Better_Health/src/Pages/User/EditProfile/EditProfile.tsx
+++ Client/Better_Health/src/Pages/User/EditProfile/EditProfile.tsx
@@ -1,9 +1,78 @@
-import React from 'react';
-import Heading from '../../../Components/Common/Heading/Heading';
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 import Input from '../../../Components/Common/Input/Input';
-import Button from '../../../Components/Common/Button/Button';
+import Button from "../../../Components/Common/Button/Button";
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import Heading from '../../../Components/Common/Heading/Heading';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 const EditProfile:React.FC = () => {
+
+    const { userID } = useParams()
+    const [Cookie, _] = useCookies(["auth_token"])
+
+    // USESTATE HOOK
+
+    const [Name, setName] = useState<string>("")
+    const [Email, setEmail] = useState<string>("")
+    const [Success, setSuccess] = useState<string>("")
+    const [Password, setPassword] = useState<string>("")
+    const [showPassword, setShowPassword] = useState<boolean>(false)
+
+    // DISPLAYING AND HIDING OF PASSWORDS
+
+    const handleTogglePassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    // CALLING ON THE USER'S DETAILS
+
+    useEffect(() => {
+
+        const FetchUser =() => {
+            try{
+                axios.get(`http://localhost:4000/Users/${userID}`, {
+                headers: { authorization: Cookie.auth_token },
+                }) 
+                .then((Data) => {
+                    setName(Data.data.Name)
+                    setEmail(Data.data.Email)
+                    setPassword(Data.data.Password) 
+                })
+            }
+            catch (Error){
+                console.log(Error)
+            }
+        }  
+        
+        FetchUser()
+
+    }, [])
+
+    // EDIT USER DETAILS FUNCTION
+
+    const EditUserDetails = (e: any) => {
+        e.preventDefault()
+
+        const data = {
+            Name, Email, Password
+        }
+        try {
+            axios.put(`http://localhost:4000/Users/${userID}`, data , {
+                headers: { authorization: Cookie.auth_token },
+            }) 
+            .then(() => { 
+                setSuccess("Details have been succesffuly edited.")
+            })
+        } catch (error) {
+            console.error(error) 
+        }
+    }
+
 return (
     <div>
         <Heading
@@ -12,32 +81,46 @@ return (
             Heading='Edit Profile'
             HeadingStyle='font-bold text-5xl'
         />
-        <form encType="multipart/form-data" className='flex flex-col items-center gap-5'>
+        <form encType="multipart/form-data" className='flex flex-col items-center gap-3'>
             <Input 
                 ContainerStyle = 'flex flex-col gap-1'
                 Label = 'Name'
                 LabelStyle = 'font-bold'
-                inputStyle = 'border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-96'         
+                inputStyle = 'border-black border-b h-5 outline-none truncate px-1 py-2 text-black w-80 sm:w-96' 
+                Value={Name}
+                Change={(e:any) => setName(e.target.value)}        
             />
             <Input 
                 ContainerStyle = 'flex flex-col gap-1'
                 Label = 'Email'
                 LabelStyle = 'font-bold'
-                inputStyle = 'border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-96'         
+                inputStyle = 'border-black border-b h-5 outline-none truncate px-1 py-2 text-black w-80 sm:w-96'
+                Value={Email}
+                Change={(e:any) => setEmail(e.target.value)}  
             />
             <Input 
-                ContainerStyle = 'flex flex-col gap-1'
+                ContainerStyle = 'flex flex-col w-80 sm:w-96'
                 Label = 'Password'
+                type={showPassword ? 'text' : 'password'}
                 LabelStyle = 'font-bold'
-                inputStyle = 'border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-96'         
+                inputStyle = 'h-5 outline-none truncate px-1 py-2 text-black w-80 sm:w-96'
+                TextStyle="border-black border-b flex flex-row"
+                Value={Password}
+                Change={(e:any) => setPassword(e.target.value)}  
+                Children={showPassword ? <FontAwesomeIcon icon={faEye} className="underline" onClick={handleTogglePassword} /> : <FontAwesomeIcon icon={faEyeSlash} className="underline" onClick={handleTogglePassword} />}
             />
-            <Button
-                ButtonText='Save Changes'
-                ButtonStyle='bg-black cursor-pointer mt-1 text-center text-white px-3 py-1 rounded w-40'
-            />
+            <div className='mt-10'>
+                <h4 className='font-bold text-center text-green-700'>{Success}</h4>
+                <Button
+                    onClick={EditUserDetails}
+                    ButtonStyle="bg-black cursor-pointer flex items-center justify-center gap-4 text-center text-white px-2 py-1.5 rounded w-40"
+                    ButtonText="Edit Details"
+                    Children={<FontAwesomeIcon icon={faPenToSquare} />}
+                />
+            </div>
         </form>
     </div>
 )
 }
-
+    
 export default EditProfile
