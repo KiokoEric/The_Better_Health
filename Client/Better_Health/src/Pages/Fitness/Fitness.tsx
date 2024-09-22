e7d3a7cd1df2bf81aa56862182f06f328c967460
“[KiokoEric]” <[kiokoerick040@gmail.com]>
2024-09-22 14:04:46 +0300

    Modification of the Fitness Page to be responsive in both mobile view and tablet view.

--- Client/Better_Health/src/Pages/Fitness/Fitness.tsx
+++ Client/Better_Health/src/Pages/Fitness/Fitness.tsx
@@ -1,38 +1,23 @@
-import React, { ChangeEvent, useState } from 'react';
-import Button from '../../Components/Common/Button/Button';
+import React, { useState } from 'react';
 import Input from '../../Components/Common/Input/Input';
+import Select from '../../Components/Common/Select/Select';
+import Button from '../../Components/Common/Button/Button';
 
 const Fitness: React.FC = () => {
 
-    const[Height, setHeight] = useState("")
-    const[Weight, setWeight] = useState("")
-    const[Activity, setActivity] = useState("")
-    const[Pounds, setPounds] = useState("")
-    const[Minutes, setMinutes] = useState("")
-    const[Calories, setCalories] = useState([])
-    const[Items, setItems]= useState([])
-    const[BMIError, setBMIError] = useState("")
-    const[CaloriesError, setCaloriesError] = useState("")
+    // USESTATE HOOK
 
-    const handleHeight = (e: ChangeEvent<HTMLInputElement>) => {
-        setHeight(e.target.value)
-    }
+    const[Items, setItems]= useState<any>([])
+    const[Calories, setCalories] = useState<[]>([])
+    const[Height, setHeight] = useState<string>("")
+    const[Weight, setWeight] = useState<string>("")
+    const[Pounds, setPounds] = useState<string>("")
+    const[Minutes, setMinutes] = useState<string>("")
+    const[BMIError, setBMIError] = useState<string>("")
+    const[Activity, setActivity] = useState<string>("")
+    const[CaloriesError, setCaloriesError] = useState<string>("")
 
-    const handleWeight = (e: ChangeEvent<HTMLInputElement>) => {
-        setWeight(e.target.value)
-    }
-
-    const handleActivity = (e: ChangeEvent<HTMLSelectElement>) => {
-        setActivity(e.target.value)
-    }
-
-    const handleMinutes = (e: ChangeEvent<HTMLInputElement>) => {
-        setMinutes(e.target.value)
-    }
-
-    const handlePounds = (e: ChangeEvent<HTMLInputElement>) => {
-        setPounds(e.target.value)
-    }
+    // BMI CALCULATOR FUNCTION
 
     const BMICalculator = async () => {
         if (Height === "" && Weight === ""){
@@ -57,6 +42,8 @@ const Fitness: React.FC = () => {
         }
         
     }
+
+    // CALORIE CALCULATOR FUNCTION
 
     const CalorieCalculator = () => {
 
@@ -85,70 +72,64 @@ const Fitness: React.FC = () => {
         }
     }
 
-
-return (
-    <div className='flex flex-col gap-10' >
-        <article className='grid grid-cols-2 gap-10 px-5 py-5'>
-            <section className='bg-LightBlue h-85 px-5 py-5 rounded-BodyMass'>
-                <h1 className='font-bold text-4xl mb-5'>
-                    Body Mass Index Calculator
-                </h1>
-                <p className='text-lg'>
-                    Body mass index is a measure of body fat based on height and weight that applies to adult men and women. It is a person’s weight in kilograms (or pounds) divided by the square of height in meters (or feet). <br /> A BMI for adult women and men between 18.5 and 24.9 is considered healthy. A BMI lower than 18.5 is considered underweight, whereas a BMI between 25.0 and 29.9 equates with overweight and 30.0 and above with obesity. <br /> BMI screens for weight categories that may lead to health problems, but it does not diagnose the body fatness or health of an individual.
-                </p>
-            </section>
-            <section className='flex flex-col gap-2 items-center justify-center' >
-                <h2 className='font-bold text-3xl'>Enter your details below</h2>
-                <div className='flex gap-20'>
-                    <form className='flex flex-col gap-3'>
-                        <div className='flex gap-2 items-end'>
+    return (
+        <div className='flex flex-col gap-5 sm:gap-10'>
+            <article className='grid grid-cols-1 gap-10 px-5 py-5 sm:grid-cols-2'>
+                <section className='bg-LightBlue h-155 px-5 py-5 rounded-BodyMass sm:h-150 md:h-120 lg:h-100 xl:h-85'>
+                    <h1 className='font-bold text-center text-4xl mb-5 xl:text-left'>
+                        Body Mass Index Calculator
+                    </h1>
+                    <p className='text-lg'>
+                        Body mass index is a measure of body fat based on height and weight that applies to adult men and women. It is a person’s weight in kilograms (or pounds) divided by the square of height in meters (or feet). <br /> A BMI for adult women and men between 18.5 and 24.9 is considered healthy. A BMI lower than 18.5 is considered underweight, whereas a BMI between 25.0 and 29.9 equates with overweight and 30.0 and above with obesity. <br /> BMI screens for weight categories that may lead to health problems, but it does not diagnose the body fatness or health of an individual.
+                    </p>
+                </section>
+                <section className='flex flex-col gap-2 items-center justify-center' >
+                    <h2 className='font-bold text-3xl'>Enter your details below</h2>
+                    <div className='flex flex-col gap-10 text-center sm:gap-20 xl:flex-row xl:text-left'>
+                        <form className='flex flex-col gap-3'>
                             <Input
                                 ContainerStyle='flex flex-col gap-2'
                                 Label='Height'
                                 LabelStyle='font-bold text-xl'
                                 Placeholder='Enter height in centimetres'
-                                inputStyle='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-52'
-                                onChange={handleHeight}
+                                inputStyle='border-black border-b h-8 outline-none px-1 py-2 text-black text-center truncate w-52'
+                                Change={(e: any) => setHeight(e.target.value)}
                             />
-                            <p>Centimetres</p>
-                        </div>
-                    </form>
-                    <form className='flex flex-col gap-3'>
-                        <div className='flex gap-2 items-end'>
+                        </form>
+                        <form className='flex flex-col gap-3'>
                             <Input
                                 ContainerStyle='flex flex-col gap-2'
                                 Label='Weight'
                                 LabelStyle='font-bold text-xl'
                                 Placeholder='Enter weight in kilograms'
-                                inputStyle='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-52'
-                                onChange={handleWeight}
+                                inputStyle='border-black border-b h-8 outline-none px-1 py-2 text-black text-center truncate w-52'
+                                Change={(e: any) => setWeight(e.target.value)}
                             />
-                        </div>
-                    </form> 
-                </div>
-                <span>{BMIError}</span>
-                <Button
-                    ButtonText='Calculate'
-                    ButtonStyle='bg-Blue cursor-pointer mt-6 py-2 rounded-sm text-center text-white w-52'
-                    onClick={BMICalculator}
-                />
-                <div className='flex gap-10 items-center justify-center mt-5'>
-                    <h3 className='font-bold text-2xl py-2 underline'>BMI Value: <span className='text-red-700'>{Items.BMI}</span> </h3>
-                    <h3 className='font-bold text-2xl py-2 underline'>Class: <span className='text-red-700' ></span>{Items.Class}</h3>
-                </div>
-            </section>
-        </article>
-        <article className='flex flex-row-reverse gap-10 px-4 py-4'>
-            <section className='bg-LightBlue h-100 px-4 py-5 rounded-Calories w-1/2'>
-                <h1 className=' font-bold text-4xl mb-5'>
-                    Calories Burned
-                </h1>
-                <p className='text-lg'>
-                    Being active is important for any weight-loss or weight-maintenance program. When you're active, your body uses more energy (calories). And when you burn more calories than you consume, you lose weight.
-                </p>
-                <h3 className='font-bold mb-5 mt-5 text-2xl'>Benefits of burning calories</h3>
-                <ul className=' flex flex-col gap-2 list-disc list-inside' >
-                    <li>
+                        </form> 
+                    </div>
+                    <span>{BMIError}</span>
+                    <Button
+                        ButtonText='Calculate'
+                        ButtonStyle='bg-Blue cursor-pointer mt-6 py-2 rounded-sm text-center text-white w-52'
+                        onClick={BMICalculator}
+                    />
+                    <div className='flex gap-10 items-center justify-center mt-5'>
+                        <h3 className='font-bold text-2xl py-2 '>BMI Value: <span className='mr-2 text-red-700 underline'>{Items.BMI}</span></h3>
+                        <h3 className='font-bold text-2xl py-2'>Class: <span className='mr-2 text-red-700 underline'>{Items.Class}</span></h3>
+                    </div>
+                </section>
+            </article>
+            <article className='flex flex-col gap-10 px-4 py-4 sm:flex-row-reverse'>
+                <section className='bg-LightBlue h-160 px-4 py-5 rounded-Calories w-full sm:w-1/2 lg:h-110'>
+                    <h1 className=' font-bold text-center text-4xl mb-5 xl:text-left'>
+                        Calories Burned
+                    </h1>
+                    <p className='text-lg'>
+                        Being active is important for any weight-loss or weight-maintenance program. When you're active, your body uses more energy (calories). And when you burn more calories than you consume, you lose weight.
+                    </p>
+                    <h3 className='font-bold mb-5 mt-5 text-center text-2xl xl:telft'>Benefits of burning calories</h3>
+                    <ul className=' flex flex-col gap-2 list-disc list-inside' >
+                        <li>
                         Ensuring your blood vessels stay healthier and lowering your risk of cardiovascular diseases.
                     </li>
                     <li>
@@ -161,73 +142,69 @@ return (
                         Helping with weight management.
                     </li>
                 </ul>
-            </section>
-            <section className='flex flex-col items-center justify-center gap-5 m-auto w-1/2' >
-                <h2 className='font-bold text-3xl'>Enter your details below</h2>
-                <div className='flex gap-5 items-center justify-center'>
-                <form  className='flex flex-col gap-2'> 
-                    <h4 className='font-bold text-xl'>Activity</h4>
-                    <div>
-                        <select name="" id="" value={Activity} onChange={handleActivity} className='border-black border-b h-8 outline-none px-2 py-1 text-black w-72'>
-                            <option value="">Search among the activities below</option>
-                            <option value="skiing">Skiing</option>
-                            <option value="Water">Water Activities</option>
-                            <option value="Run">Walking/Running</option>
-                            <option value="Rope">Jumping Rope</option>
-                            <option value="Horse">Horse Riding</option>
-                            <option value="Golf">Golf</option>
-                            <option value="Calisthenics">Calisthenics</option>
-                            <option value="Aerobics">Aerobics</option>
-                            <option value="cycling">Cycling</option>
-                        </select>
+                </section>
+                <section className='flex flex-col items-center justify-center gap-5 m-auto text-center sm:w-1/2 xl:text-left'>
+                    <h2 className='font-bold text-3xl'>Enter your details below</h2>
+                    <div className='flex flex-col gap-5 items-center justify-center xl:flex-row'>
+                        <form  className='flex flex-col gap-2'> 
+                            <h4 className='font-bold text-lg'>Activity</h4>
+                            <div>
+                                <Select SelectStyle='border-black border-b h-8 outline-none px-2 py-1 text-black text-center w-80 xl:w-72' Search={Activity} onSearch={e => setActivity(e.target.value)} >
+                                    <option value="">Search among the activities below</option>
+                                    <option value="skiing">Skiing</option>
+                                    <option value="Water">Water Activities</option>
+                                    <option value="Run">Walking/Running</option>
+                                    <option value="Rope">Jumping Rope</option>
+                                    <option value="Horse">Horse Riding</option>
+                                    <option value="Golf">Golf</option>
+                                    <option value="Calisthenics">Calisthenics</option>
+                                    <option value="Aerobics">Aerobics</option>
+                                    <option value="cycling">Cycling</option>
+                                </Select>
+                            </div>
+                        </form>
+                        <form className='flex flex-col gap-3'>
+                            <Input
+                                ContainerStyle='flex flex-col gap-2'
+                                Label='Weight'
+                                LabelStyle='font-bold text-xl'
+                                Placeholder='Enter weight in pounds'
+                                inputStyle='border-black border-b h-8 outline-none px-1 py-2 text-black text-center truncate w-80 xl:w-64'
+                                Change={(e: any)=> setPounds(e.target.value)}
+                            />
+                        </form> 
                     </div>
-                </form>
-                <form className='flex flex-col gap-3'>
-                    <div className='flex gap-2 items-end'>
-                        <Input
-                            ContainerStyle='flex flex-col gap-2'
-                            Label='Weight'
-                            LabelStyle='font-bold text-xl'
-                            Placeholder='Enter weight in pounds'
-                            inputStyle='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-64'
-                            onChange={handlePounds}
-                        />
-                        <p>Pounds</p>
-                    </div>
-                </form> 
-                </div>
-                <form className='flex flex-col justify-center gap-2'>
-                    <div className='flex gap-2 items-end'>
+                    <form className='flex flex-col justify-center gap-2'>
                         <Input
                             ContainerStyle='flex flex-col gap-2'
                             Label='Duration'
-                            LabelStyle='font-bold text-xl'
-                            Placeholder='Enter duration'
-                            inputStyle='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-96'
-                            onChange={handleMinutes}
+                            LabelStyle='font-bold text-lg'
+                            Placeholder='Enter duration in minutes'
+                            inputStyle='border-black border-b h-8 outline-none  px-1 py-2 text-black text-center truncate w-80 xl:w-96'
+                            Change={(e: any) => setMinutes(e.target.value)}
                         />
-                        <p>Minutes</p>
-                    </div>
-                </form>
-                <span>{CaloriesError}</span>
-                <Button
-                    ButtonText='Calculate'
-                    ButtonStyle='bg-Blue cursor-pointer py-2 rounded-sm text-center text-white w-52'
-                    onClick={CalorieCalculator}
-                />
-                {Calories.map((Calorie: any) => {
-                    return (
-                        <div className='flex gap-8' >
-                            <h4 className='capitalize font-bold text-xl'>Activity: {Calorie.name}</h4>
-                            <h4 className='capitalize font-bold text-xl'>Calories: {Calorie.total_calories}</h4>
-                        </div>
-                    )
-                })
-                }
-            </section>
-        </article>
-    </div>
-)
+                    </form>
+                    <span>{CaloriesError}</span>
+                    <Button
+                        ButtonText='Calculate'
+                        ButtonStyle='bg-Blue cursor-pointer py-2 rounded-sm text-center text-white w-52'
+                        onClick={CalorieCalculator}
+                    />
+                    <section className='flex flex-col justify-center gap-5'>
+                        {Calories.map((Calory: any) => {
+                            return (
+                                <div className='grid grid-cols-2 items-center justify-center gap-5'>
+                                    <h4 className='capitalize font-bold text-lg underline'>Activity: {Calory.name}</h4>
+                                    <h4 className='capitalize font-bold text-lg underline'>Calories: {Calory.total_calories}</h4>
+                                    </div>
+                            )
+                        })
+                        }
+                    </section>
+                </section>
+            </article>
+        </div>
+    )
 }
 
 export default Fitness
