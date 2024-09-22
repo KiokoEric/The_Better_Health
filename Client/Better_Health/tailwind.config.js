b77eff93ad523d3e3cdbe208859cb330ac9a5297
“[KiokoEric]” <[kiokoerick040@gmail.com]>
2024-09-22 21:19:24 +0300

    Introduction of more customised Tailwind themes to the Tailwind Configuration file.

--- Client/Better_Health/tailwind.config.js
+++ Client/Better_Health/tailwind.config.js
@@ -14,12 +14,20 @@ export default {
         'custom-dark': '1px 1px 4px black',
       },
       borderRadius: {
+        'Header': '0px 0px 0px 5px',
         'BodyMass': '0px 0px 160px 0px',
-        'Calories': '0px 0px 0px 110px'  // Adds a new custom radius value
+        'Calories': '0px 0px 0px 110px'
       },
       height: {
-        '85': '22rem', // Custom height value
-        '100': '26rem'
+        '85': '22rem', 
+        '100': '26rem',
+        '120': '32rem',
+        '150': '35rem',
+        '155': '37rem',
+        '160': '40rem'
+      },
+      width: {
+        'custom': '85vw',
       },
     },
   },
