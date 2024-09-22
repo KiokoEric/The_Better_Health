b707ac5c3934341aee3b9802500e78186872caa1
“[KiokoEric]” <[kiokoerick040@gmail.com]>
2024-09-22 19:50:18 +0300

    Modification of the Login Page to be responsive in both mobile view and tablet view.

--- Client/Better_Health/src/Pages/User/Registration/Registration.tsx
+++ Client/Better_Health/src/Pages/User/Registration/Registration.tsx
@@ -1,12 +1,11 @@
 import * as z from 'zod';
-import Axios from "axios";
+import axios from "axios";
 import React from 'react';
 import { useSnackbar } from 'notistack';
-import { useCookies } from "react-cookie";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import Heading from '../../../Components/Common/Heading/Heading';
 import Button from '../../../Components/Common/Button/Button';
+import Heading from '../../../Components/Common/Heading/Heading';
 
 const Registration: React.FC = () => {
 
@@ -16,30 +15,32 @@ const Registration: React.FC = () => {
         Password: string;
     };
 
+    // CREATION OF THE REGISTRATION ZOD SCHEMA
+
     const RegistrationSchema = z.object({
         Name: z.string().min(1, { message: 'Name is required'}),
         Email: z.string().email({ message: "Invalid email address" }),
         Password: z.string().min(1, { message: 'Password is required'})
     });
 
-    const [_,setCookie] = useCookies(["auth_token"]);
     const { enqueueSnackbar } = useSnackbar();
 
     const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({
         resolver: zodResolver(RegistrationSchema)
     });
 
-    const onRegistration : SubmitHandler<FormValues> = async (data) => {
+    type UserData = z.infer<typeof RegistrationSchema>
+
+    // ONREGISTRATION FUNCTION
+
+    const onRegistration : SubmitHandler<FormValues> = async (data: UserData) => {
         try {
-            const response = await Axios.post("https://localhost:4000/Users/Registration", data) 
-                setCookie("auth_token", response.data.Token)
-                window.localStorage.setItem("UserID", response.data.UserID)
-                enqueueSnackbar("Logged in successfully!", {variant: "success"}) 
-                setTimeout(() => {
-                    window.location.reload();
-                }, 1500);
+            await axios.post(`http://localhost:4000/Users/Registration`, data) 
+            .then(() => {
+                enqueueSnackbar("Registration Completed! Kindly Log in", {variant: "success"})
+            })
         } catch (error) { 
-            enqueueSnackbar("Login unsuccessful!" , {variant: "error"})
+            enqueueSnackbar("Registration unsuccessful!" , {variant: "error"})
             console.log(error)
         }
     }
@@ -55,17 +56,17 @@ return (
         <form method="post" onSubmit={handleSubmit(onRegistration)} encType="multipart/form-data" className='flex flex-col items-center gap-2'>
             <div className='flex flex-col gap-2'>
                 <label className='font-bold' htmlFor="">Name</label> 
-                <input placeholder="Enter Name..." {...register('Name', { required: 'Name is required' })} className='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-96' required />
+                <input placeholder="Enter Name..." {...register('Name', { required: 'Name is required' })} className='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-80 sm:w-96' required />
                 {errors.Name && <p className="text-center text-red-700">{errors.Name.message}</p>}
             </div>
             <div className='flex flex-col gap-2'>
                 <label className='font-bold' htmlFor="Email">Email</label> 
-                <input placeholder="Enter Email..." {...register('Email', { required: 'Email is required' })} className='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-96' required />
+                <input placeholder="Enter Email..." {...register('Email', { required: 'Email is required' })} className='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-80 sm:w-96' required />
                 {errors.Email && <p className="text-center text-red-700">{errors.Email.message}</p>}
             </div>
             <div className='flex flex-col gap-2'>
-                <label className='font-bold' htmlFor="Password">Passowrd</label> 
-                <input placeholder="Enter Password..." {...register('Password', { required: 'Password is required' })} className='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-96' required />
+                <label className='font-bold' htmlFor="Password">Password</label> 
+                <input placeholder="Enter Password..." {...register('Password', { required: 'Password is required' })} className='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-80 sm:w-96' required />
                 {errors.Password && <p className="text-center text-red-700">{errors.Password.message}</p>}
             </div>
             <Button
