4822f508f17e9a9759581a3fa46b490389a21c0d
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-10-14 17:28:06 +0300

    Update of the Exercise Route File

--- Server/Routes/ExerciseRoute.js
+++ Server/Routes/ExerciseRoute.js
@@ -1,40 +1,84 @@
+const dotenv = require('dotenv');
 const express = require('express');
-const FavouriteRoute = express.Router();
-const Favourite = require("../Models/Favourites");
+const jwt = require("jsonwebtoken");
+const ExerciseRoute = express.Router();
+const Workout = require("../Models/Workout");
 const cookieParser = require("cookie-parser");
 
-FavouriteRoute.use(cookieParser())
+ExerciseRoute.use(cookieParser())
+dotenv.config();
 
-// ADDING A FAVOURITE
+const myPassword = process.env.Password
 
-FavouriteRoute.post("/AddFavourite", async (req, res) => {
-    const Exercises = new Favourite(req.body)
+const verifyToken = async (req, res, next) => {
+
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        jwt.verify(authHeader, myPassword, (err) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+        next();
+        });
+    } else {
+        return res.status(401).send("Authorisation token is missing!");
+    }
+}
+
+// ADDING A WORKOUT
+
+ExerciseRoute.post("/AddWorkout", verifyToken ,async (req, res) => {
+    const Workouts = new Workout(req.body)
 
     try {
-        const SavedExercises = await Exercises.save() 
-        res.send(SavedExercises)
+        const SavedWorkout = await Workouts.save() 
+        res.send(SavedWorkout)
     } catch (error) {
         console.error(error)
     }
 })
 
-// GETTING ALL THE FAVOURITES CREATED BY A SINGLE USER BY THEIR USER ID
+// GETTING ALL THE WORKOUTS CREATED BY ALL THE USERS
 
-FavouriteRoute.get('/:userId/Favourites', async (req, res) => {
+ExerciseRoute.get("/AllWorkouts", async (req, res) => { 
+    try{
+        const AllWorkouts = await Workout.find() 
+        res.json(AllWorkouts)
+    }
+    catch(err) { 
+        res.send(err)  
+    }
+})
+
+// GETTING ALL THE WORKOUTS CREATED BY A SINGLE USER BY THEIR USER ID
+
+ExerciseRoute.get('/:userId/Workout', async (req, res) => {
     const userId = req.params.userId;
     try {
-        const Favourites = await Favourite.find({ userOwner: userId });
-        res.json(Favourites);
+        const Workouts = await Workout.find({ userOwner: userId });
+        res.json(Workouts);
     } catch (error) {
-        res.status(500).json({ message: 'Error fetching favourites.' });
+        res.status(500).json({ message: 'Error fetching workouts.' });
     }
 });
 
-// DELETING A FAVOURITE BASED ON THE FAVOURITE ID
+// UPDATING A WORKOUT BASED ON THE WORKOUT ID
 
-FavouriteRoute.delete("/:id", async (req, res) => {
+ExerciseRoute.put("/:id", async (req, res) => {
     try{
-        const Favourites = await Favourite.findByIdAndDelete(req.params.id)
+        const Workouts = await Workout.findByIdAndUpdate(req.params.id, req.body)
+        res.json(Workouts)
+    }
+    catch(err) {
+        res.send(err)
+    }
+})
+
+// DELETING A WORKOUT BASED ON THE WORKOUT ID
+
+ExerciseRoute.delete("/:id", async (req, res) => {
+    try{
+        const Workouts = await Workout.findByIdAndDelete(req.params.id)
         res.json({Message: "Deleted Successfully!"})
     }
     catch(err) {
@@ -42,4 +86,18 @@ FavouriteRoute.delete("/:id", async (req, res) => {
     }
 })
 
-module.exports = FavouriteRoute
+// GETTING A WORKOUT BY ITS ID
+
+ExerciseRoute.get('/MyWorkouts/:id', async (req, res) => {
+    try {
+    const Workouts = await Workout.findById(req.params.id);
+    if (!Workouts) {
+        return res.status(404).json({ message: 'Workout not found' });
+    }
+    res.json(Workouts);
+    } catch (error) {
+    res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+module.exports = ExerciseRoute 
